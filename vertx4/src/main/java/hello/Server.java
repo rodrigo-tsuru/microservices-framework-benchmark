7175cc268e09474cf328d538cbddd2a8cc35c3de
Rodrigo Tsuru <caixapostal@gmail.com>
2021-07-27 23:22:19 +0000

    initial commit

---
+++ vertx4/src/main/java/hello/Server.java
@@ -0,0 +1,33 @@
+package hello;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.*;
+
+public class Server extends AbstractVerticle {
+
+    private static final int INSTANCES = Runtime.getRuntime().availableProcessors() * 2;
+    private static final Buffer TEXT = Buffer.buffer("Hello World!");
+
+    private static final String TEXT_PLAIN = "text/plain";
+    private static final String VERTX = "Vertx";
+
+    // Convenience method so you can run it in your IDE
+    public static void main(String[] args) {
+        DeploymentOptions options = new DeploymentOptions().setInstances(INSTANCES);
+        Vertx.vertx(new VertxOptions().setPreferNativeTransport(true)).deployVerticle(Server.class.getName(), options);
+    }
+
+    @Override
+    public void start() throws Exception {
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response()
+                .putHeader(HttpHeaders.SERVER, VERTX)
+                .putHeader(HttpHeaders.CONTENT_TYPE, TEXT_PLAIN)
+                .end(TEXT);
+        }).listen(8080);
+    }
+}
